9fa4faa3f5fdce44114977b171a31e66c6f92b7f
user17-02 <jerosha78@gmail.com>
2026-03-04 10:39:07 +0530

    changed footer color

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,16 @@
+import Header from './components/Header/Header'
 import Hero from './components/Hero'
+import Footer from './components/Footer/Footer'
 import './App.css'
 
 function App() {
   return (
     <>
+      <Header />
       <Hero />
+      <Footer />
     </>
   )
 }
 
-export default App
+export default App;
